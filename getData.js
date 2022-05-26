e503b3781552aef016185cc422c8282f11d37050
Logan <ultimate360solutions@gmail.com>
2022-05-26 12:58:33 -0500

    reworked data cache functions to use native fetch()

--- getData.js
+++ getData.js
@@ -1,4 +1,3 @@
-const rp = require('request-promise');
 const fs = require('fs');
 const console = require('node:console');
 const chalk = require('chalk');
@@ -11,83 +10,85 @@ const { JSDOM } = jsdom;
 
 
 // Collects data for U.S. states
-function usStatsCacher() {
-  rp('https://www.worldometers.info/coronavirus/country/us/')
-    .then(function (nice) {
-      const dom = new JSDOM(nice);
-      let jsonArr = [];
-      const table = dom.window.document.getElementById('usa_table_countries_today').tBodies.item(0).rows;
-      for (let i = 0; i < table.length; i++) {
-        const cells = table[i].cells;
-        const JSONbuilder = {
-          'state': cells[1].textContent.trim(),
-          'cases': cells[2].textContent.trim(),
-          'newcases': cells[3].textContent.trim(),
-          'deaths': cells[4].textContent.trim(),
-          'newdeaths': cells[5].textContent.trim(),
-          'recoveries': cells[6].textContent.trim(),
-          'activecases': cells[7].textContent.trim(),
-          'casesbypop': cells[8].textContent.trim(),
-          'deathsbypop': cells[9].textContent.trim(),
-          'testsperformed': cells[10].textContent.trim(),
-          'testsperformedbypop': cells[11].textContent.trim()
-        };
-        jsonArr.push(JSONbuilder);
-      }
-      fs.writeFile('USstats.json', JSON.stringify(jsonArr), function (err) {
-        if (err)
-          return console.log(err);
-      });
-      console.log(chalk.magenta('Successfully cached US data!!'));
-    })
-    .catch(function (err) {
-      console.log(chalk.red('US cache retrieval error: ') + err);
+async function usStatsCacher() {
+  const res = await fetch('https://www.worldometers.info/coronavirus/country/us/');
+  if (res.ok) {
+    const data = await res.text();
+    const dom = new JSDOM(data);
+    let jsonArr = [];
+    const table = dom.window.document.getElementById('usa_table_countries_today').tBodies.item(0).rows;
+    for (let i = 0; i < table.length; i++) {
+      const cells = table[i].cells;
+      const JSONbuilder = {
+        'state': cells[1].textContent.trim(),
+        'cases': cells[2].textContent.trim(),
+        'newcases': cells[3].textContent.trim(),
+        'deaths': cells[4].textContent.trim(),
+        'newdeaths': cells[5].textContent.trim(),
+        'recoveries': cells[6].textContent.trim(),
+        'activecases': cells[7].textContent.trim(),
+        'casesbypop': cells[8].textContent.trim(),
+        'deathsbypop': cells[9].textContent.trim(),
+        'testsperformed': cells[10].textContent.trim(),
+        'testsperformedbypop': cells[11].textContent.trim()
+      };
+      jsonArr.push(JSONbuilder);
+    }
+    fs.writeFile('USstats.json', JSON.stringify(jsonArr), function (err) {
+      if (err)
+        return console.log(err);
     });
+    console.log(chalk.magenta('Successfully cached US data!!'));
+  }
+  else {
+    console.log(chalk.red('US cache retrieval error: ') + res.status);
+  }
 }
 
 // Collects data for countries worldwide and overall global data
-function worldMetersCacher(){
-  rp('https://www.worldometers.info/coronavirus/')
-    .then(function (nice) {
-      const dom = new JSDOM(nice);
-      let jsonArr = [];
-      const totalCases = dom.window.document.getElementsByClassName('maincounter-number')[0].textContent.trim();
-      const totalDeaths = dom.window.document.getElementsByClassName('maincounter-number')[1].textContent.trim();
-      const totalRecovered = dom.window.document.getElementsByClassName('maincounter-number')[2].textContent.trim();
-      const totalActiveCases = dom.window.document.getElementsByClassName('number-table-main')[0].textContent.trim();
-      const JSONbuilder = {
-        'totalCases': totalCases,
-        'totalDeaths': totalDeaths,
-        'totalRecovered': totalRecovered,
-        'totalActiveCases': totalActiveCases
+async function worldMetersCacher() {
+  const res = await fetch('https://www.worldometers.info/coronavirus/');
+  if (res.ok) {
+    const data = await res.text();
+    const dom = new JSDOM(data);
+    let jsonArr = [];
+    const totalCases = dom.window.document.getElementsByClassName('maincounter-number')[0].textContent.trim();
+    const totalDeaths = dom.window.document.getElementsByClassName('maincounter-number')[1].textContent.trim();
+    const totalRecovered = dom.window.document.getElementsByClassName('maincounter-number')[2].textContent.trim();
+    const totalActiveCases = dom.window.document.getElementsByClassName('number-table-main')[0].textContent.trim();
+    const JSONbuilder = {
+      'totalCases': totalCases,
+      'totalDeaths': totalDeaths,
+      'totalRecovered': totalRecovered,
+      'totalActiveCases': totalActiveCases
+    };
+    jsonArr.push(JSONbuilder);
+    const table = dom.window.document.getElementById('main_table_countries_today').tBodies.item(0).rows;
+    for (let i = 0; i < table.length; i++) {
+      const cells = table[i].cells;
+      const JSONbuilder2 = {
+        'country': cells[1].textContent.trim(), //remove occasional newline characters
+        'cases': cells[2].textContent,
+        'newcases': cells[3].textContent,
+        'deaths': cells[4].textContent.trim(), //deaths number has strange trailing whitespace, so it's trimmed here
+        'newdeaths': cells[5].textContent,
+        'recovered': cells[6].textContent,
+        'activecases': cells[8].textContent,
+        'criticalcases': cells[9].textContent,
+        'testsperformed': cells[12].textContent,
+        'testsperformedbypop': cells[13].textContent
       };
-      jsonArr.push(JSONbuilder);
-      const table = dom.window.document.getElementById('main_table_countries_today').tBodies.item(0).rows;
-      for (let i = 0; i < table.length; i++) {
-        const cells = table[i].cells;
-        const JSONbuilder2 = {
-          'country': cells[1].textContent.trim(), //remove occasional newline characters
-          'cases': cells[2].textContent,
-          'newcases': cells[3].textContent,
-          'deaths': cells[4].textContent.trim(), //deaths number has strange trailing whitespace, so it's trimmed here
-          'newdeaths': cells[5].textContent,
-          'recovered': cells[6].textContent,
-          'activecases': cells[8].textContent,
-          'criticalcases': cells[9].textContent,
-          'testsperformed': cells[12].textContent,
-          'testsperformedbypop': cells[13].textContent
-        };
-        jsonArr.push(JSONbuilder2);
-      }
-      fs.writeFile('WorldStats.json', JSON.stringify(jsonArr), function (err) {
-        if (err)
-          return console.log(err);
-      });
-      console.log(chalk.magenta('Successfully cached world data!!'));
-    })
-    .catch(function (err) {
-      console.log(chalk.red('World cache retrieval error: ') + err);
+      jsonArr.push(JSONbuilder2);
+    }
+    fs.writeFile('WorldStats.json', JSON.stringify(jsonArr), function (err) {
+      if (err)
+        return console.log(err);
     });
+    console.log(chalk.magenta('Successfully cached world data!!'));
+  }
+  else {
+    console.log(chalk.red('World cache retrieval error: ') + res.status);
+  }
 }
 
 exports.worldCache = worldMetersCacher;
